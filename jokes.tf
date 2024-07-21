3405bf58fbfb541131ef9520e6d7cd88fdce0ade
Brent Rogers <brentarogers@gmail.com>
2024-07-21 15:35:30 -0400

    fixed 3

--- jokes.tf
+++ jokes.tf
@@ -9,7 +9,7 @@ module "jokes" {
   function_name     = "jokes"
   function_filename = "${path.cwd}/functions/jokes.py"
   valid_users       = "all"
-  invoke_string     = "joke"
+  invoke_string     = "send_joke"
   router_role_name  = aws_iam_role.message_router.id
   env               = var.env
   layers            = [module.jokes_layer.arn]
--- modules/python_layer/main.tf
+++ modules/python_layer/main.tf
@@ -31,7 +31,7 @@ data "archive_file" "this" {
 resource "aws_lambda_layer_version" "this" {
   layer_name          = var.layer_name
   filename            = data.archive_file.this.output_path
-  source_code_hash    = sha256(data.archive_file.this.output_base64sha256)
+  source_code_hash    = sha256(var.requirements)
   compatible_runtimes = var.runtimes
 }
 
