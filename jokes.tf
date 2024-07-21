fb226793590581c17331190efe82a48523e2df10
Brent Rogers <brentarogers@gmail.com>
2024-07-21 09:28:01 -0400

    try 3

--- jokes.tf
+++ jokes.tf
@@ -1,5 +1,5 @@
 module "jokes_layer" {
   source            = "./modules/python_layer"
   layer_name        = "pyjokes"
-  requirements_path = "./functions/jokes/requirements.txt"
+  requirements_path = "functions/jokes/requirements.txt"
 }
