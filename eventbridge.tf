b5ef123722374802200b51bb63e63b73e8a53d43
Brent Rogers <brentarogers@gmail.com>
2024-07-22 11:36:08 -0400

    hunting for arn outside the handler

---
+++ eventbridge.tf
@@ -0,0 +1,4 @@
+# resource "aws_cloudwatch_event_rule" "new_function" {
+#   name = "new-chatbot-function"
+#   description = "Catch when a new lambda is deployed that matches the condition"
+# }
--- functions/jokes.py
+++ functions/jokes.py
@@ -1,4 +1,14 @@
+import boto3
+import os
 from pyjokes import get_joke
+
+ParameterInjector()
 
 def lambda_handler(event, context):
     print(get_joke())
+
+
+def ParameterInjector():
+    client = boto3.client('sts')
+    response = client.get_caller_identity()
+    print(response)
