aa7d55b66ecd973cac3ce82a045cb9ef339a2527
Brent Rogers <brentarogers@gmail.com>
2024-07-22 11:10:43 -0400

    go to pre-built zips to simplify process

--- jokes.tf
+++ jokes.tf
@@ -1,8 +1,16 @@
-module "jokes_layer" {
-  source     = "./modules/python_layer"
-  layer_name = "pyjokes"
-  requirements = "pyjokes >= 0.6.0"
+# module "jokes_layer" {
+#   source     = "./modules/python_layer"
+#   layer_name = "pyjokes"
+#   requirements = "pyjokes >= 0.6.0"
+# }
+
+resource "aws_lambda_layer_version" "pyjokes" {
+  layer_name          = "pyjokes"
+  filename            = "./layers/pyjokes.zip"
+  source_code_hash    = filebase64sha256("./layers/pyjokes.zip")
+  compatible_runtimes = ["python3.12"]
 }
+
 
 module "jokes" {
   source            = "./modules/chatbot_function"
@@ -12,5 +20,5 @@ module "jokes" {
   invoke_string     = "send_joke"
   router_role_name  = aws_iam_role.message_router.id
   env               = var.env
-  layers            = [module.jokes_layer.arn]
+  layers            = [aws_lambda_layer_version.pyjokes.arn]
 }
