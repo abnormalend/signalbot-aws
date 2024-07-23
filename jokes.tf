695836aa4dc6db30ed4a9e2cb3e143ff35e72f66
Brent Rogers <brentarogers@gmail.com>
2024-07-23 07:44:59 -0400

    reformat

--- jokes.tf
+++ jokes.tf
@@ -1,10 +1,21 @@
+#Jokes is the "hello world" of chatbot functions, and mostly serves as a reference to the process to deploy a function
+
 module "jokes" {
+# These top values in general won't change from function to function.
   source            = "./modules/chatbot_function"
-  function_name     = "jokes"
-  function_file_path = "${path.cwd}/functions"
-  valid_users       = "all"
-  invoke_string     = "send_joke"
+  function_file_path = "${path.cwd}/functions"   
   router_role_name  = aws_iam_role.message_router.id
   env               = var.env
+
+# What do we name the function?  This is in AWS, not in the chatbot messages.  This is also the name of the function (minus .py)
+  function_name     = "jokes"                               
+  
+# Who can call the function?  This is a work in progress
+  valid_users       = "all"
+
+# What /command starts the message that invokes this function.  In this case /send_joke will trigger this function
+  invoke_string     = "send_joke"
+
+# If the function requires additional python modules, we need to build a layer.  Lambda has few by default (boto3)
   layers            = [aws_lambda_layer_version.pyjokes.arn]
 }
